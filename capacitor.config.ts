7b50620a368d9ae66cec0fe0dfe04eec96731339
Alexander Ryu Fenando <alxanderryu777@gmail.com>
2024-04-27 19:18:48 +0700

    petterinary app initialization and typo fix

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'petterinary',
+  webDir: 'dist/petterinary',
+  bundledWebRuntime: false,
+};
+
+export default config;
