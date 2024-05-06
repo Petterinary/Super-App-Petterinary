da53fbee8ef1fcd02df50b848049e888535511db
Daevan <115785730+DaevanM@users.noreply.github.com>
2024-05-06 18:52:36 +0700

    update

--- src/app/component/app.component.ts
+++ src/app/component/app.component.ts
@@ -6,15 +6,4 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  public landingPage = true;
-
-  constructor(private router: Router) {}
-
-  ngOnInit() {
-    setTimeout(() => {
-      this.landingPage = false;
-      this.router.navigate(['/main-menu']);
-    }, 5000);
-  }
-}
+export class AppComponent {}
