f9f07668417a5abe02aba822f888c5eff75bc5d9
Alexander Ryu Fenando <alxanderryu777@gmail.com>
2024-05-16 16:55:14 +0700

    new global module back button module

--- src/app/component/global/global.module.ts
+++ src/app/component/global/global.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderBarComponent } from './header-bar/header-bar.component';
 import { BottomBarComponent } from './bottom-bar/bottom-bar.component';
 import { IonicModule } from '@ionic/angular';
+import { BackButtonComponent } from './back-button/back-button.component';
 
 @NgModule({
-  declarations: [HeaderBarComponent, BottomBarComponent],
+  declarations: [HeaderBarComponent, BottomBarComponent, BackButtonComponent],
   imports: [IonicModule, CommonModule],
-  exports: [HeaderBarComponent, BottomBarComponent],
+  exports: [HeaderBarComponent, BottomBarComponent, BackButtonComponent],
 })
 export class GlobalModule {}
