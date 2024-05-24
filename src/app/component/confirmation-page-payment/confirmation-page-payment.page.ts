bacf76484885904e104bbb7a0fc2bcb7ccd844ea
Alexander Ryu Fenando <alxanderryu777@gmail.com>
2024-05-24 18:48:31 +0700

    update confirm page for payment

---
+++ src/app/component/confirmation-page-payment/confirmation-page-payment.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmation-page-payment',
+  templateUrl: './confirmation-page-payment.page.html',
+  styleUrls: ['./confirmation-page-payment.page.scss'],
+})
+export class ConfirmationPagePaymentPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
