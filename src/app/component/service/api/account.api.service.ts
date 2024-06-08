bfb6a024bcb97f94cfb62fd51d19cf74c24832d3
Nats742 <nathan.sidharta@binus.ac.id>
2024-06-08 15:04:52 +0700

    Update Account api
    Added CreateDoctorAccount
    Added GetByDoctorID

--- src/app/component/service/api/account.api.service.ts
+++ src/app/component/service/api/account.api.service.ts
@@ -19,12 +19,19 @@ export class AccountService {
     return this.http.get(`${this.apiUrl}/accounts/${accountID}`);
   }
 
+  getDoctorAccountById(doctorID: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/accounts/doctor/${doctorID}`);
+  }
+
   getAccountByUid(uid: string): Observable<any> {
     return this.http.get(`${this.apiUrl}/accounts/uid/${uid}`);
   }
 
   createAccount(account: any): Observable<any> {
     return this.http.post(`${this.apiUrl}/accounts/create`, account);
+  }
+  createDoctorAccount(account: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/accounts/createDoctorAccount`, account);
   }
 
   updateAccount(accountID: number, account: any): Observable<any> {
