89f666e79c2ede24a3d5fd8b169e55a0e602617d
Daevan <115785730+DaevanM@users.noreply.github.com>
2024-06-26 20:36:33 +0700

    update env

--- src/environtments/environment.prod.ts
+++ src/environtments/environment.prod.ts
@@ -1,16 +1,15 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://petterinary.onrender.com', // deployed URL for production
+  apiUrl: 'https://petterinary.onrender.com',
   googleMapsApiKey: 'AIzaSyAT2BSJnYwn9CTMhqu9Xx8xpVE8TC3JxI0',
   // googleMapsApiKey: '',
-  //   apiUrl: 'http://localhost:4000',
   firebase: {
-    apiKey: 'AIzaSyD9w1VyW4ZlP9fbiRnpgGT7QUA3WDLDVpQ',
-    authDomain: 'petterinary-d431b.firebaseapp.com',
-    projectId: 'petterinary-d431b',
-    storageBucket: 'petterinary-d431b.appspot.com',
-    messagingSenderId: '621596239461',
-    appId: '1:621596239461:web:dd8b236306b15cceda888c',
-    measurementId: 'G-64NSZ2ZPE5',
+    apiKey: 'AIzaSyB9twhERMr9MlUa_stal9uFjZwxEAj83G4',
+    authDomain: 'petterinary-4adb2.firebaseapp.com',
+    projectId: 'petterinary-4adb2',
+    storageBucket: 'petterinary-4adb2.appspot.com',
+    messagingSenderId: '726530622609',
+    appId: '1:726530622609:web:9189de345811d419eaeb30',
+    measurementId: 'G-7SXZLD8CKE',
   },
 };
